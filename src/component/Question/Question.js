f45a02bcae6ac4c83927793a9b246d48b1ef59c3
shafayat zamil <zamilkhan111.za@gmail.com>
2022-09-29 03:04:24 +0600

    question 1 add

---
+++ src/component/Question/Question.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Question.css';
+
+const Question = () => {
+    return (
+        <div>
+            <h1>How does react work?</h1>
+            <p>react is a libraries not fomeWork.it has jsx,virtual dom,props and state,component.JSX is a JavaScript syntax extension used in React element creation.React can apply updates to specific parts of the data tree, which is faster than re-rendering the entirety of the DOM tree.single pices in component and when it send and recive it is props.It changes whenever a user interacts with the application, rendering a new UI </p>
+            <h1>Difference between props and state</h1>
+            <p></p>
+            <h1>useEffect use </h1>
+        </div>
+    );
+};
+
+export default Question;
